fd4a6262496df2a6e521ae678b0c07072e22617e
Randy Ridenour <>
2020-09-13 19:58:50 -0500

    Modify lecture function to fit emacs function.

--- fish/functions/lecture.fish
+++ fish/functions/lecture.fish
@@ -1,14 +1,14 @@
 function lecture
     mkdir {$argv}
     cd {$argv}
-    touch {$argv}.org
-    echo -e '#+startup: beamer' \n'#+TITLE: ' \n'#+AUTHOR: Dr. Ridenour' \n'#+DATE: ddate' >>{$argv}.org
-    touch {$argv}-beamer.org
-    cat /Users/rlridenour/Dropbox/emacs/beamer/lecture-beamer.org >{$argv}-beamer.org
-    echo -e '#+include: "'{$argv}'.org" :minlevel 1' >>{$argv}-beamer.org
+    touch {$argv}-data.org
+    echo -e '#+startup: beamer' \n'#+TITLE: ' \n'#+AUTHOR: Dr. Ridenour' \n'#+DATE: ddate' >>{$argv}-data.org
+    touch {$argv}-slides.org
+    cat /Users/rlridenour/Dropbox/emacs/beamer/lecture-beamer.org >{$argv}-slides.org
+    echo -e '#+include: "'{$argv}'-data.org" :minlevel 1' >>{$argv}-slides.org
     touch {$argv}-notes.org
     cat /Users/rlridenour/Dropbox/emacs/beamer/lecture-notes.org >{$argv}-notes.org
-    echo -e '#+include: "'{$argv}'.org" :minlevel 1' >>{$argv}-notes.org
+    echo -e '#+include: "'{$argv}'-data.org" :minlevel 1' >>{$argv}-notes.org
     dired
     open -a emacs
 end
