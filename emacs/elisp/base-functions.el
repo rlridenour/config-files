82e2a2b918a3b80af9eb41c1aecc5b0c50ef5835
Randall Ridenour <rlridenour@MacBook-Air.local>
2021-09-05 09:27:15 -0500

    Add pop-up shell.

--- emacs/elisp/base-functions.el
+++ emacs/elisp/base-functions.el
@@ -300,6 +300,30 @@
 (global-set-key (kbd "C-`") 'iterm-goto-filedir-or-home)
 
 
+;; Create pop-up shell with F12 key
+;; From https://tsdh.wordpress.com/2011/10/12/a-quick-pop-up-shell-for-emacs/
+
+(defvar th-shell-popup-buffer nil)
+
+(defun th-shell-popup ()
+  "Toggle a shell popup buffer with the current file's directory as cwd."
+  (interactive)
+  (unless (buffer-live-p th-shell-popup-buffer)
+    (save-window-excursion (shell "*Popup Shell*"))
+    (setq th-shell-popup-buffer (get-buffer "*Popup Shell*")))
+  (let ((win (get-buffer-window th-shell-popup-buffer))
+	(dir (file-name-directory (or (buffer-file-name)
+				      ;; dired
+				      dired-directory
+				      ;; use HOME
+				      "~/"))))
+    (if win
+	(quit-window nil win)
+      (pop-to-buffer th-shell-popup-buffer nil t)
+      (comint-send-string nil (concat "cd " dir "\n")))))
+
+(global-set-key (kbd "<f12>") 'th-shell-popup)
+
 ;; Saved Keyboard Macros
 
 ;; Splits Org-mode list items
