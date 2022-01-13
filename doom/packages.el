b55adfbfacdbe81ced79695237a237344b8f2f50
Randy Ridenour <rlridenour@gmail.com>
2022-01-13 17:21:49 -0600

    Combine beamer note snippets.

--- doom/packages.el
+++ doom/packages.el
@@ -53,7 +53,6 @@
  :recipe (:host gitlab :repo "protesilaos/modus-themes" :branch "main"))
 (package! hungry-delete)
 (package! wc-mode)
-;; (package! org-ref)
 (package! major-mode-hydra)
 (package! shrink-whitespace)
 (package! deadgrep)
@@ -62,7 +61,6 @@
 ;; (package! org-roam-bibtex
   ;; :recipe (:host github :repo "org-roam/org-roam-bibtex"))
 (package! dired-narrow)
-;; (package! fish-mode)
 ;; (package! grab-mac-link
   ;; :recipe (:host github :repo "xuchunyang/grab-mac-link.el"))
 (package! doom-snippets :ignore t)
@@ -70,8 +68,7 @@
 (package! crux)
 ;; (package! org-contrib
 ;;   :recipe (:host nil :repo "https://git.sr.ht/~bzg/org-contrib"))
-;; (package! ebib)
+(package! ebib)
 (package! ox-hugo)
-;; (package! citeproc-org)
 (package! unfill)
 (package! key-chord)
