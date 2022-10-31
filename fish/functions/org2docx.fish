9474dfb0abc270f86cad7b9c333b60130589df0a
Randy Ridenour <rlridenour@gmail.com>
2022-10-31 07:30:16 -0500

    Unsuccessfully fix org2docx.

--- fish/functions/org2docx.fish
+++ fish/functions/org2docx.fish
@@ -1,5 +1,5 @@
 function org2docx --description 'Generate a DOCX file using a custom reference document'
-    set -l refdoc "$PATH_TO_REFERENCE_DOCX"
+    set -l refdoc "custom-reference.docx"
     set -l base (basename -s .md -s .org $argv)
     echo Generating $base.docx ...
     pandoc --reference-doc $refdoc -o $base.docx $argv
