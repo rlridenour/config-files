44e19400df8ceb6b7eb1c7ed639f62dcdff6e7e0
Randy Ridenour <rlridenour@gmail.com>
2023-05-15 07:31:26 -0500

    Set theme to Doom Plain.

--- rmacs/elisp/theme.el
+++ rmacs/elisp/theme.el
@@ -18,7 +18,7 @@
   ;; Global settings (defaults)
   (setq doom-themes-enable-bold t    ; if nil, bold is universally disabled
         doom-themes-enable-italic t) ; if nil, italics is universally disabled
-  (load-theme 'doom-opera-light t)
+  (load-theme 'doom-plain t)
 
   ;; Enable flashing mode-line on errors
   (doom-themes-visual-bell-config)
