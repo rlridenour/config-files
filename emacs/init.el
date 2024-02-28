8882399e3afb48daa682a50d5f26573e8feec132
Randy Ridenour <rlridenour@gmail.com>
2024-02-28 08:48:19 -0600

    Add trackpad buffer switching.

--- emacs/init.el
+++ emacs/init.el
@@ -371,6 +371,33 @@ Version 2016-06-19"
  "M-<tab>" #'xah-next-emacs-buffer
  )
 
+(defvar *my-previous-buffer* t
+  "can we switch?")
+
+(defun my-previous-buffer ()
+  (interactive)
+  (message "custom prev: *my-previous-buffer*=%s" *my-previous-buffer*)
+  (when *my-previous-buffer*
+    (previous-buffer)
+    (setq *my-previous-buffer* nil)
+    (run-at-time "1 sec" nil (lambda ()
+                               (setq *my-previous-buffer* t)))))
+
+(defvar *my-next-buffer* t
+  "can we switch?")
+
+(defun my-next-buffer ()
+  (interactive)
+  (message "custom prev: *my-next-buffer*=%s" *my-next-buffer*)
+  (when *my-next-buffer*
+    (next-buffer)
+    (setq *my-next-buffer* nil)
+    (run-at-time "1 sec" nil (lambda ()
+                               (setq *my-next-buffer* t)))))
+
+(global-set-key [triple-wheel-right] 'my-previous-buffer)
+(global-set-key [triple-wheel-left] 'my-next-buffer)
+
 (setq initial-scratch-message nil
       initial-major-mode 'org-mode)
 
@@ -2090,10 +2117,7 @@ after it is inserted."
   :general
   ( "<f6>" #'yankpad-insert))
 
-(use-package osx-dictionary
-:bind
-("")
-)
+(use-package osx-dictionary)
 
 (setq async-shell-command-buffer "new-buffer")
 
